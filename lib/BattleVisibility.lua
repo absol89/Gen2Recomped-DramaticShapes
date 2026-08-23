c5e5544cd58d1e8efc26e74b353bcd247db0f498
absol89 <absol89@users.noreply.github.com>
2026-08-23 19:36:32 +0200

    Remove incorrect intro animation-layer suppression

--- lib/BattleVisibility.lua
+++ lib/BattleVisibility.lua
@@ -21,8 +21,7 @@ function BattleVisibility.sideVisible(battle, side)
 end
 
 function BattleVisibility.animationLayerVisible(battle)
-  if not battle then return true end
-  return not (battle.introBalls or (tonumber(battle.introSlide) or 0) > 0)
+  return not (battle and battle.introBalls)
 end
 
 return BattleVisibility
