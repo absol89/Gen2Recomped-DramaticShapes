e0e08a7f04515fd99aa3c530aa46f8604321be1b
absol89 <absol89@users.noreply.github.com>
2026-08-23 22:05:12 +0200

    Key ROM front sprite white to transparency

--- lib/OverworldBattle.lua
+++ lib/OverworldBattle.lua
@@ -1118,7 +1118,9 @@ function OverworldBattle.install()
        or shinyPic then
       return out
     end
-    if OverworldBattle.isFrontPokemonPic(self, img) then return out end
+    if OverworldBattle.isFrontPokemonPic(self, img) then
+      return BattlePics.transparentWhite(out)
+    end
     return BattlePics.filled(out, OverworldBattle.pinnedPic(self, img))
   end
 
